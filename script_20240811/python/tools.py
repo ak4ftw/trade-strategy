41dc495125e89ed3e0018f730e17642f7c830ce0
ak4ftw <490216135@qq.com>
2024-08-11 19:59:19 +0800

    删除 邮件发送功能 移动到 tools.py

--- script_20240811/python/tools.py
+++ script_20240811/python/tools.py
@@ -3,6 +3,10 @@ import hashlib
 import math
 import vndb
 import pmodel
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
 
 # 时间处理
 def get_now_time():
@@ -61,3 +65,44 @@ def is_today_open():
     if count > 0:
         return True
     return False
+
+
+# 发送文本邮件
+def send_email_text(toEmail, title, text):
+    # 邮件信息
+    from_email = kv_get("send_email_username")
+    to_email = toEmail
+    subject = title
+    body = text
+
+    # 设置SMTP服务器及端口
+    smtp_server = kv_get("send_email_smtp_server")
+    smtp_port = kv_get("send_email_smtp_port")
+    password = kv_get("send_email_password")
+
+    # 创建MIMEMultipart对象
+    msg = MIMEMultipart()
+    msg["From"] = from_email
+    msg["To"] = to_email
+    msg["Subject"] = subject
+
+    # 将邮件正文添加到MIMEMultipart对象中
+    msg.attach(MIMEText(body, "plain"))
+
+    # 发送邮件
+    try:
+        # 连接到SMTP服务器
+        server = smtplib.SMTP(smtp_server, smtp_port)
+        server.starttls()  # 启用安全传输模式
+        server.login(from_email, password)
+
+        # 发送邮件
+        server.sendmail(from_email, to_email, msg.as_string())
+        print("Email sent successfully!")
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        # 关闭服务器连接
+        server.quit()
+
+    pass
